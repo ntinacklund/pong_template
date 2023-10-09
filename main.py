5e69a98c6961531d403ce65d783e6c75335d48a8
Niclas L <niclas.lund@ntig.se>
2023-10-09 16:12:04 +0200

    Added base files

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+# din screen bör vara rektanguljär, ex. 800x600
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ scoreboard.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 80, "normal")
+POSITION = ()
+
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        pass
